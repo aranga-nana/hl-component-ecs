2bf7a35326274115b145e15911e829b7d57da2c2
arangananayakkara <arangananayakkara@fintrix.com.au>
2019-09-22 13:13:25 +1000

    clean up

--- ecs.cfndsl.rb
+++ ecs.cfndsl.rb
@@ -110,7 +110,7 @@ CloudFormation do
       user_data << "set -x\n"
       user_data << "until curl -s http://localhost:51678/v1/metadata\n"
       user_data << "do\n"
-      user_data << "   sleep 1\n"
+      user_data << " sleep 1\n"
       user_data << "done\n"
       user_data << "docker plugin install rexray/ebs REXRAY_PREEMPT=true EBS_REGION="
       user_data << Ref("AWS::Region")
